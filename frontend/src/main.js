060427db89be1d5cd1be187620ad95643d5f095c
yanjun0501 <i@yanjun-forever.cn>
2019-01-25 16:08:02 +0800

    feat: add socket.io

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import io from 'socket.io-client';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(io('http://localhost:3002'));
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
